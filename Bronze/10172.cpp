4d7279bc1aa9a4158825fe68f0e831842e5d1045
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2023-11-17 13:55:32 +0900

    개

---
+++ Bronze/10172.cpp
@@ -0,0 +1,13 @@
+// 10172.cpp 개
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|";
+}
