127e86f2e95ba77ba8509127f2d8964c668135d7
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2023-11-17 14:08:42 +0900

    A+B - 4

---
+++ Bronze/10951.cpp
@@ -0,0 +1,16 @@
+// 10951.cpp A+B - 4
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int A, B, sum;
+
+    while ((cin >> A >> B))
+    {
+
+        cout << A + B << '\n';
+    }
+    return 0;
+}
