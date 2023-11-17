af9c8ea0445fd70350cb8c81109f6ea680dda181
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2023-11-17 14:12:55 +0900

    좌표 정렬하기 2

---
+++ Silver/11651.cpp
@@ -0,0 +1,42 @@
+// 11651.cpp 좌표 정렬하기 2
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    if (a.second == b.second)
+    {
+        return a.first < b.first;
+    }
+    else
+    {
+        return a.second < b.second;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> v;
+    pair<int, int> p;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> p.first >> p.second;
+        v.push_back(p);
+    }
+
+    sort(v.begin(), v.end(), compare);
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << v[i].first << ' ' << v[i].second << '\n';
+    }
+}
