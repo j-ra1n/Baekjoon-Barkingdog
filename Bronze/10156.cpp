f3706ee706e9167d9d4dd35366a50c953f4c747c
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2023-11-18 17:37:32 +0900

    과자

---
+++ Bronze/10156.cpp
@@ -0,0 +1,21 @@
+// 10156.cpp 과자
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if ((a * b) < c)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << a * b - c;
+    }
+}
