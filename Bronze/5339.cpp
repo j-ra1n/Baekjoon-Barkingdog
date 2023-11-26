bf866fbdf9ce0ce1e732c8d79599018a8177f4fa
j-ra1n <xw21yog@dankook.ac.kr>
2023-11-26 19:50:37 +0900

    콜센터

---
+++ Bronze/5339.cpp
@@ -0,0 +1,18 @@
+// 5339.cpp 콜센터
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "     /~\\" << '\n';
+    cout << "    ( oo|" << '\n';
+    cout << "    _\\=/_" << '\n';
+    cout << "   /  _  \\" << '\n';
+    cout << "  //|/.\\|\\\\ " << '\n';
+    cout << " ||  \\ /  ||" << '\n';
+    cout << "============" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+}
