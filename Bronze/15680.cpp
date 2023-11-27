630b0d64a6a031572dd293457f1bf22c091b18c1
j-ra1n <xw21yog@dankook.ac.kr>
2023-11-27 10:07:18 +0900

    연세대학교

---
+++ Bronze/15680.cpp
@@ -0,0 +1,20 @@
+// 15680.cpp 연세대학교
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    if (n == 0)
+    {
+        cout << "YONSEI";
+    }
+    else
+    {
+        cout << "Leading the Way to the Future";
+    }
+}
