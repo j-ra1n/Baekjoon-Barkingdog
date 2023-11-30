b92de9793062064107b941ba573807be0e949661
j-ra1n <xw21yog@dankook.ac.kr>
2023-11-30 17:09:21 +0900

    제리와 톰

---
+++ Bronze/16430.cpp
@@ -0,0 +1,14 @@
+// 16430.cpp 제리와 톰 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int a, b; cin >> a >> b;
+
+	cout << b - a << " " << b << "\n";
+}
