815bc3ad7f3afe55b904ed03a6fbb2aff58575a5
j-ra1n <xw21yog@dankook.ac.kr>
2023-12-06 18:04:58 +0900

    수학은 체육과목 입니다 2

--- Bronze/17362.cpp
+++ Bronze/17362.cpp
@@ -2,14 +2,16 @@
 
 #include <iostream>
 using namespace std;
-int main() {
+int main()
+{
     int n;
     cin >> n;
-    if (n <= 5) cout << n << '\n';
+    if (n <= 5)
+        cout << n << '\n';
     else
     {
         if ((n - 5) / 4 % 2 == 0)
-            cout << 5 - (n-5) % 4 << '\n';
+            cout << 5 - (n - 5) % 4 << '\n';
         else
             cout << 1 + (n - 5) % 4 << '\n';
     }
