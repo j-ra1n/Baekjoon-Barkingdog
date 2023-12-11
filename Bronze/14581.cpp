32321658cd595da0214aa2038136efb2998cb448
j-ra1n <xw21yog@dankook.ac.kr>
2023-12-11 15:53:41 +0900

    팬들에게 둘러싸인 홍준

---
+++ Bronze/14581.cpp
@@ -0,0 +1,39 @@
+// 14581.cpp 팬들에게 둘러싸인 홍준
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string name;
+
+    cin >> name;
+
+    char em[6] = ":fan:";
+
+    for (int i = 0; i < 3; i++)
+
+    {
+
+        for (int j = 0; j < 3; j++)
+
+        {
+
+            if (i == 1 && j == 1)
+            {
+
+                cout << ":" << name << ":";
+            }
+
+            else
+
+                cout << em;
+        }
+
+        cout << "\n";
+    }
+}
