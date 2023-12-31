c7b233808d32fa3b412d2d492a9f070fc61766a1
j-ra1n <j_ra1n@naver.com>
2023-12-31 17:47:08 +0900

    설탕 배달

---
+++ Silver/2839.cpp
@@ -0,0 +1,25 @@
+// 2839.cpp 설탕 배달
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int cnt = 0;
+    
+    while (n >= 0)
+    {
+        if (n % 5 == 0)
+        {
+            cnt += (n / 5);
+            cout << cnt << '\n';
+            return 0;
+        }
+        n -= 3;
+        cnt++;
+    }
+    cout << -1 << '\n';
+}
