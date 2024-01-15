174178b775c8db6cada0e390cabae45f4bd9745f
j-ra1n <j_ra1n@naver.com>
2024-01-15 19:53:41 +0900

    큰수의법칙.cpp

---
+++ 이코테/그리디/큰수의법칙.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, k;
+    int cnt = 0, res = 0;
+    cin >> n >> m >> k;
+
+    vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+
+    while (1)
+    {
+        if (m == cnt)
+        {
+            break;
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            res += v[n - 1];
+            cnt++;
+        }
+        res += v[n - 2];
+        cnt++;
+    }
+
+    cout << res;
+}
