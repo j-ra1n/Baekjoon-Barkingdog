218c873fdaa1f9f767efa3801b3eff7f8b703130
j-ra1n <j_ra1n@naver.com>
2024-01-23 21:54:44 +0900

    시각

---
+++ 이코테/구현/시각.cpp
@@ -0,0 +1,38 @@
+// 시각.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool check(int h, int m, int s)
+{
+    if (h % 10 == 3 || m % 10 == 3 || m / 10 == 3 || s % 10 == 3 || s / 10 == 3)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    int N;
+    int cnt = 0;
+    cin >> N;
+    for (int i = 0; i <= N; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            for (int k = 0; k < 60; k++)
+            {
+                if (check(i, j, k))
+                {
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    cout << cnt;
+}
