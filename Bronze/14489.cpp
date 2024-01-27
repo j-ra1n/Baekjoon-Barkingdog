1c5ae111a476e05e2bb1880c9519442a23f1b28c
j-ra1n <j_ra1n@naver.com>
2024-01-27 21:22:55 +0900

    치킨 두 마리 (...)

---
+++ Bronze/14489.cpp
@@ -0,0 +1,24 @@
+// 144489.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+    if (a + b - 2 * c >= 0){
+
+        cout << a + b - 2 * c << '\n';
+    }
+    else
+    {
+
+        cout << a + b << '\n';
+    }
+}
