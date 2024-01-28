e00bc31de4b8d6246b3d6bf85c8d8c826f9d82d1
j-ra1n <j_ra1n@naver.com>
2024-01-29 00:27:18 +0900

    재귀함수가 뭔가요?

---
+++ 재귀/17478.cpp
@@ -0,0 +1,66 @@
+// 17478.cpp 재귀함수가 뭔가요?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int cnta = 0;
+
+void under_bar(int c)
+{
+    for (int i = 0; i < c; i++)
+    {
+        cout << "____";
+    }
+}
+
+void text_a()
+{
+    if (cnta == n)
+    {
+        return;
+    }
+
+    under_bar(cnta);
+    cout << "\"재귀함수가 뭔가요?\"" << '\n';
+    under_bar(cnta);
+    cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << '\n';
+    under_bar(cnta);
+    cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << '\n';
+    under_bar(cnta);
+    cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << '\n';
+    cnta++;
+    text_a();
+}
+
+void text_b(int cntb)
+{
+    if (cntb == 0)
+    {
+        cout << "라고 답변하였지.";
+        return;
+    }
+
+    under_bar(cntb);
+    cout << "라고 답변하였지." << '\n';
+    text_b(--cntb);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    int cntb = n;
+
+    cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << '\n';
+    text_a();
+    under_bar(cntb);
+    cout << "\"재귀함수가 뭔가요?\"" << '\n';
+    under_bar(cntb);
+    cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << '\n';
+    text_b(cntb);
+}
+
