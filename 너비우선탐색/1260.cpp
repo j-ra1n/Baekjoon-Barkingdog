c74cb8188bfe5dc5839f231942ccf63b8786da8d
j-ra1n <j_ra1n@naver.com>
2024-02-13 21:23:19 +0900

    DFS와 BFS

--- 너비우선탐색/1260.cpp
+++ 너비우선탐색/1260.cpp
@@ -1,23 +1,22 @@
 // 1260.cpp DFS와 BFS
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
+vector<int> graph[1001];
 bool visited_dfs[1001];
 bool visited_bfs[1001];
-vector<int> graph_dfs[1001];
-vector<int> graph_bfs[1001];
 int n, m, v;
+queue<int> q;
 
 void dfs(int x)
 {
     visited_dfs[x] = true;
     cout << x << ' ';
 
-    for (int i = 0; i < graph_dfs[x].size(); i++)
+    for (int i = 0; i < graph[x].size(); i++)
     {
-        int node = graph_dfs[x][i];
+        int node = graph[x][i];
 
         if (!visited_dfs[node])
         {
@@ -28,38 +27,25 @@ void dfs(int x)
 
 void bfs(int x)
 {
-    queue<int> q;
     q.push(x);
     visited_bfs[x] = true;
+    cout << x << ' ';
 
     while (!q.empty())
     {
         x = q.front();
         q.pop();
-        cout << x << ' ';
 
-        for (int i = 0; i < graph_bfs[x].size(); i++)
+        for (int i = 0; i < graph[x].size(); i++)
         {
-            int node = graph_bfs[x][i];
-
+            int node = graph[x][i];
             if (!visited_bfs[node])
             {
                 q.push(node);
                 visited_bfs[node] = true;
+                cout << node << ' ';
             }
         }
-    }
-}
-
-bool compare(const pair<int, int> &a, const pair<int, int> &b)
-{
-    if (a.first == b.first)
-    {
-        return a.second < b.second;
-    }
-    else
-    {
-        return a.first < b.first;
     }
 }
 
@@ -70,22 +56,18 @@ int main()
     cout.tie(0);
 
     cin >> n >> m >> v;
-
-    vector<pair<int, int>> p(m);
-
+    
     for (int i = 0; i < m; i++)
     {
-        cin >> p[i].first >> p[i].second;
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
-    sort(p.begin(), p.end(), compare);
 
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= n; i++)
     {
-
-        graph_dfs[p[i].first].push_back(p[i].second);
-        graph_dfs[p[i].second].push_back(p[i].first);
-        graph_bfs[p[i].first].push_back(p[i].second);
-        graph_bfs[p[i].second].push_back(p[i].first);
+        sort(graph[i].begin(), graph[i].end());
     }
 
     dfs(v);
