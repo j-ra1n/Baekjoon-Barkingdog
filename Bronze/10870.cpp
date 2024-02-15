ccbcc19df4fe4a9cfb7e4bc0d55a264e2e12fd65
j-ra1n <j_ra1n@naver.com>
2024-02-15 21:35:54 +0900

    피보나치 수 5

---
+++ Bronze/10870.cpp
@@ -0,0 +1,29 @@
+// 10870.cpp 피보나치 수 5
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fibo(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << fibo(n);
+}
